b061fbd684a9dcfef057ee272fc52fc05c33b7dc
MD-khan <md.monir.khan707@gmail.com>
2021-12-12 23:09:53 -0500

    Create lambda function
    This is a real simple lambda function for counting total words in a text file and sending the result to email subscribers.
    How does it work?
    When a text file is being uploaded to an s3 bucket, this lambda function gets triggered and finally, AWS SNS sends the result to the email address.

---
+++ lambda_function.py
@@ -0,0 +1,33 @@
+import json
+import urllib.parse
+import boto3
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+sns = boto3.client('sns')
+
+
+def lambda_handler(event, context):
+    # Get object - this is the s3 bucket name
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    
+    # Get the key - this is the file name
+    key = event['Records'][0]['s3']['object']['key']
+    
+    # Get response
+    response = s3.get_object(Bucket=bucket, Key=key)
+    
+    # Check the file type - looking for only plain/text
+    # Read the  file content
+    file_content = str(response['Body'].read())
+    
+    words = file_content.split()
+    total_words = len(words)
+    message = "The word count in the file {} is {}".format(key, total_words)
+    # Send email
+    sns_response = sns.publish(
+        TargetArn='arn:aws:sns:us-west-2:318846086362:Lambda-word-count',
+        Message=message,
+        Subject="Word Count Result"
+    )
